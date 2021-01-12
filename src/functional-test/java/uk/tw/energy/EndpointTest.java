c125406e6b6b77a1a9657dccba2898c7eb28171f
Peter Weismann <peter.weismann@integrationworks.co.th>
2021-01-12 17:00:36 +0700

    Improve functional test using mockmvc instead of testresttemplate and improve test case naming

--- src/functional-test/java/uk/tw/energy/EndpointTest.java
+++ src/functional-test/java/uk/tw/energy/EndpointTest.java
@@ -1,82 +1,102 @@
 package uk.tw.energy;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import uk.tw.energy.builders.MeterReadingsBuilder;
 import uk.tw.energy.domain.MeterReadings;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = App.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = App.class)
+@AutoConfigureMockMvc
 public class EndpointTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
-    @Autowired
     private ObjectMapper mapper;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    public void shouldStoreReadings() throws JsonProcessingException {
+    public void shouldStoreReadings() throws Exception {
         MeterReadings meterReadings = new MeterReadingsBuilder().generateElectricityReadings().build();
-        HttpEntity<String> entity = getStringHttpEntity(meterReadings);
 
-        ResponseEntity<String> response = restTemplate.postForEntity("/readings/store", entity, String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        mockMvc.perform(
+                post("/readings/store")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        mapper.writeValueAsString(meterReadings)
+                )
+        )
+        .andDo(print())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andReturn();
     }
 
     @Test
-    public void givenMeterIdShouldReturnAMeterReadingAssociatedWithMeterId() throws JsonProcessingException {
+    public void givenMeterIdShouldReturnAMeterReadingAssociatedWithMeterId() throws Exception {
         String smartMeterId = "bob";
         populateMeterReadingsForMeter(smartMeterId);
 
-        ResponseEntity<String> response = restTemplate.getForEntity("/readings/read/" + smartMeterId, String.class);
+        mockMvc.perform(
+                get("/readings/read/{smartMeterId}", smartMeterId)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    public void shouldCalculateAllPrices() throws JsonProcessingException {
+    public void shouldCalculateAllPrices() throws Exception {
         String smartMeterId = "bob";
         populateMeterReadingsForMeter(smartMeterId);
 
-        ResponseEntity<String> response = restTemplate.getForEntity("/price-plans/compare-all/" + smartMeterId, String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        mockMvc.perform(
+                get("/price-plans/compare-all/{smartMeterId}", smartMeterId)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
     }
 
     @Test
-    public void givenMeterIdAndLimitShouldReturnRecommendedCheapestPricePlans() throws JsonProcessingException {
+    public void givenMeterIdAndLimitShouldReturnRecommendedCheapestPricePlans() throws Exception {
         String smartMeterId = "bob";
         populateMeterReadingsForMeter(smartMeterId);
 
-        ResponseEntity<String> response =
-                restTemplate.getForEntity("/price-plans/recommend/" + smartMeterId + "?limit=2", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        mockMvc.perform(
+                get("/price-plans/compare-all/{smartMeterId}/?limit={limit}", smartMeterId, 2)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
     }
 
-    private HttpEntity<String> getStringHttpEntity(Object object) throws JsonProcessingException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        String jsonMeterData = mapper.writeValueAsString(object);
-        return (HttpEntity<String>) new HttpEntity(jsonMeterData, headers);
-    }
-
-    private void populateMeterReadingsForMeter(String smartMeterId) throws JsonProcessingException {
+    private void populateMeterReadingsForMeter(String smartMeterId) throws Exception {
         MeterReadings readings = new MeterReadingsBuilder().setSmartMeterId(smartMeterId)
                 .generateElectricityReadings(20)
                 .build();
 
-        HttpEntity<String> entity = getStringHttpEntity(readings);
-        restTemplate.postForEntity("/readings/store", entity, String.class);
+        mockMvc.perform(
+                post("/readings/store")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        mapper.writeValueAsString(readings)
+                )
+        )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
     }
 }
